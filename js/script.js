f05630e5ac30e19d3fb55eccec2c9ee3a8941d16
Venkat Polumahanti <venkat4541@gmail.com>
2017-01-27 20:34:46 -0500

    DONE - Nav bar animation on scroll
    TODO
    - Update welcome section
    - Vector graphics where required
    - Colors where required
    - Logo gif
    - Font for headings
    - Support mobile view
    - Fix footer bugs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
 /**
  * Created by Venkat on 1/10/2017.
  */
-$(window).scroll(function() {
-    if($(document).scrollTop() > 50) {
-        $(header).style("height", "35px")
-    }
+$(".resume").animatedModal();
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    window.addEventListener('scroll',function(e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300;
+
+        if(distanceY > shrinkOn) {
+            $('header').addClass('minimize');
+            $('#logo').addClass('logo-min');
+            $('.nav-item').addClass('nav-item-min');
+        } else {
+            if($('header').hasClass('minimize')) {
+                $('header').removeClass('minimize');
+            }
+            if($('#logo').hasClass('logo-min')) {
+                $('#logo').removeClass('logo-min');
+            }
+            if($('li').hasClass('nav-item-min')) {
+                $('li').removeClass('nav-item-min');
+            }
+        }
+
+    });
 });
